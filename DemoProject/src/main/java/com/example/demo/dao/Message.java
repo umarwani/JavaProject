ad1533f442acb368d71eba137040b549b3d481d7
umarw <umarw@DESKTOP-EMMDAO3>
2021-01-05 21:40:06 +0530

    Intial Commit

---
+++ DemoProject/src/main/java/com/example/demo/dao/Message.java
@@ -0,0 +1,14 @@
+package com.example.demo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message 
+{
+	public void printMessage()
+	{
+		System.out.println("here is the message");
+	}
+	
+
+}
